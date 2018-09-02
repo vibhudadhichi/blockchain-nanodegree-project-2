62fa99d284b49692780a46b0e45a381223888696
Yury Prokashev <yury.prokashev@gmail.com>
2018-09-02 13:36:29 +0300

    - added getter for 'db' to BlockchainDb class - added getter for 'chain' to Blockchain class - rejections in Blockchain and BlockchainDb always return Error instance - fixed uncaught rejection in BlockchainDb.isEmpty - renamed 'db' member of Blockchain class to 'chain' - got rid of 'log' - addBlock now returns Promise - getBlock now return Promise - fixed validateChain to return array of errors with all failed Blocks.

--- simpleChain.js
+++ simpleChain.js
@@ -1,25 +1,28 @@
 /* ===== SHA256 with Crypto-js ===============================
-|  Learn more: Crypto-js: https://github.com/brix/crypto-js  |
-|  =========================================================*/
+ |  Learn more: Crypto-js: https://github.com/brix/crypto-js  |
+ |  =========================================================*/
 const SHA256 = require('crypto-js/sha256');
 
 /* ===== Persistent Key-value store with level DB ============
-|                                                            |
-|  =========================================================*/
+ |                                                            |
+ |  =========================================================*/
 const level = require('level');
 /* ===== console.log wrapper =======================
-|  Wrapper for console.log.                        |
-|  ===============================================*/
+ |  Wrapper for console.log.                        |
+ |  ===============================================*/
 function log(message) {
     console.log(`LOG: ${message}\n`);
 }
 
 /* ===== BlockchainDb Class ========================
-|  Wrapper for leveldb.                            |
-|  ===============================================*/
+ |  Wrapper for leveldb.                            |
+ |  ===============================================*/
 class BlockchainDb {
     constructor(dbDir) {
         this.db = level(dbDir);
+    }
+    getDb() {
+        return this.db;
     }
     saveBlock(block) {
         let _this = this;
@@ -27,10 +30,9 @@ class BlockchainDb {
         return new Promise(function(resolve, reject) {
             _this.db.put(key, JSON.stringify(block), function(err){
                 if(err) {
-                    log('Block ' + key + ' submission failed\n'+ err);
-                    resolve(false);
+                    reject(new Error(`Block ${key} submission failed. ${err.message}`));
                 }
-                resolve(true);
+                resolve(block);
             })
         });
     }
@@ -39,11 +41,10 @@ class BlockchainDb {
         return new Promise(function(resolve, reject) {
             _this.db.get(key, function(err, value) {
                 if(err) {
-                    log("Can not get Block at key=" + key);
-                    reject(err);
+                    reject(new Error(`Can not get Block at key = ${key}. ${err.message}`));
+                } else {
+                    resolve(JSON.parse(value));
                 }
-                // log(`block at index ${key} is:\n${value}`);
-                resolve(JSON.parse(value));
             });
         });
     }
@@ -56,8 +57,7 @@ class BlockchainDb {
                     length++;
                 })
                 .on('error', function(err) {
-                    log(err);
-                    reject(err);
+                    reject(new Error(`Error in DB Read Stream. ${err.message}`));
                 })
                 .on('close', function(){
                     resolve(length);
@@ -70,20 +70,20 @@ class BlockchainDb {
             let length = _this.getChainLength();
             length.then(result => {
                 if(result === 0) {
-                    log("DB is empty");
                     resolve(true);
                 } else {
-                    log("DB is not empty");
                     resolve(false);
                 }
+            }).catch(err => {
+                reject(new Error(`Can not determine, if DB is empty. ${err.message}`));
             });
         });
     }
-}
+};
 
 /* ===== Block Class ==============================
-|  Class with a constructor for block 			   |
-|  ===============================================*/
+ |  Class with a constructor for block 			   |
+ |  ===============================================*/
 
 class Block{
     constructor(data){
@@ -96,110 +96,124 @@ class Block{
 }
 
 /* ===== Blockchain Class ==========================
-|  Class with a constructor for new blockchain 		|
-|  ================================================*/
+ |  Class with a constructor for new blockchain 		|
+ |  ================================================*/
 
 class Blockchain{
     constructor(db){
-        this.db = db;
-        db.isEmpty().then(result => {
+        this.chain = db || new BlockchainDb("./db");
+        this.chain.isEmpty().then(result => {
             if(result) {
-                log("Blockchain DB is empty. Creating new Blockchain with 1 genesis block...");
+                console.log("Blockchain DB is empty. Creating new Blockchain with 1 genesis block...");
                 this.addBlock(new Block("First block in the chain - Genesis block"));
             } else {
-                log("Blockchain DB has blocks. Reading Blockchain from DB...");
+                console.log("Blockchain DB has blocks. Reading Blockchain from DB...");
             }
         }).catch(err => {
-            log(err);
+            throw err;
         });
     }
 
+    getChain() {
+        return this.chain;
+    }
+
     addBlock(newBlock){
-        newBlock.time = new Date().getTime().toString().slice(0,-3);
-        this.db.getChainLength().then(chainLength => {
-            newBlock.height = chainLength;
-            if(chainLength === 0) {
-                return new Promise((resolve, reject) => {
-                    log("chain length = 0, return null instead of block");
-                    resolve(null);
-                });
-            } else {
-                log(`chain length is ${chainLength}, return previous block`);
-                return this.db.getBlock(chainLength - 1);
-            }
-        }).then(previousBlock => {
-            if(previousBlock === null) {
-                newBlock.previousBlockHash = "";
-            } else {
-                newBlock.previousBlockHash = previousBlock.hash;
-            }
-            newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
-            return this.db.saveBlock(newBlock);
-        }).then(saveOperationResult => {
-            if(saveOperationResult === true) {
-                log("block saved");
-            } else {
-                log("block is not saved");
-            }
-        }).catch(err => {
-            log(err);
+        let _this = this;
+        return new Promise((resolve, reject) => {
+            newBlock.time = new Date().getTime().toString().slice(0,-3);
+            _this.chain.getChainLength().then(chainLength => {
+                newBlock.height = chainLength;
+                if(chainLength === 0) {
+                    return new Promise((resolve, reject) => {
+                        console.log("chain length = 0, return null instead of block");
+                        resolve(null);
+                    });
+                } else {
+                    console.log(`chain length is ${chainLength}, return previous block`);
+                    return _this.chain.getBlock(chainLength - 1);
+                }
+            }).then(previousBlock => {
+                if(previousBlock === null) {
+                    newBlock.previousBlockHash = "";
+                } else {
+                    newBlock.previousBlockHash = previousBlock.hash;
+                }
+                newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
+                return _this.chain.saveBlock(newBlock);
+            }).then(saveOperationResult => {
+                console.log("block saved");
+                resolve(saveOperationResult);
+            }).catch(err => {
+                reject(new Error(`${err.message}`));
+            });
         });
     }
 
     getBlockHeight() {
-        this.db.getChainLength().then(currentLength => {
-            return currentLength;
-        }).catch(err => {
-            log(err);
+        let _this = this;
+        return new Promise((resolve, reject) => {
+            _this.chain.getChainLength().then(currentLength => {
+                resolve(currentLength);
+            }).catch(err => {
+                reject(new Error(`${err.message}`));
+            });
         });
     }
 
     getBlock(blockHeight){
-        this.db.getBlock(blockHeight).then(block => {
-            log(JSON.stringify(block));
-            return block;
-        }).catch(err => {
-            log(err);
+        return new Promise((resolve, reject) => {
+            this.chain.getBlock(blockHeight).then(block => {
+                resolve(block);
+            }).catch(err => {
+                reject(new Error(`${err.message}`));
+            });
         });
     }
 
     validateBlock(blockHeight){
         let _this = this;
         return new Promise(function(resolve, reject){
-            _this.db.getBlock(blockHeight).then(block => {
+            _this.chain.getBlock(blockHeight).then(block => {
                 let blockHash = block.hash;
                 block.hash = '';
                 let validBlockHash = SHA256(JSON.stringify(block)).toString();
                 if (blockHash === validBlockHash) {
                     resolve(true);
                 } else {
-                    log('Block #'+blockHeight+' invalid hash:\n'+blockHash+'<>'+validBlockHash);
-                    resolve(false);
+                    reject(new Error('Block #'+blockHeight+' invalid hash:\n'+blockHash+'<>'+validBlockHash));
                 }
             });
         });
     }
 
     validateChain() {
-        let allBlocks = [];
+        let errors = [];
         let _this = this;
-        this.db.getChainLength().then(currentLength => {
-            for(let i = 0; i < currentLength; i++) {
-                allBlocks.push(_this.validateBlock(i));
-            }
-            return Promise.all(allBlocks);
-        }).then(validationResults => {
-            let errors = [];
-            validationResults.forEach((validation, index) => {
-                if(validation === false) errors.push(index);
-            });
-            if(errors.length === 0) {
-                log(`${validationResults.length} blocks checked. Chain is valid.`);
-            } else {
-                log(`Chain is invalid. Invalid blocks heights are: ${errors}`);
-            }
-        }).catch(err => {
-            log(err);
+        return new Promise((resolve, reject) => {
+            _this.chain.getChainLength()
+                .then(currentLength => {
+                    let allBlockValidations = [];
+                    for(let i = 0; i < currentLength; i++) {
+                        allBlockValidations.push(
+                            _this.validateBlock(i)
+                                .catch(err => {
+                                    errors.push(err);
+                                })
+                        );
+                    }
+                    return Promise.all(allBlockValidations);
+                })
+                .then(value => {
+                    if(errors.length > 0) {
+                        reject(errors);
+                    } else {
+                        resolve(true);
+                    }
+                })
+                .catch(err => {
+                    reject(err);
+                });
         });
     }
 }
