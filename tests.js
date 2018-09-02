93c548f7e90fe825acbe3eea9c811a9f4e7dcc54
Yury Prokashev <yury.prokashev@gmail.com>
2018-09-02 13:29:17 +0300

    got rid of 'log' and moved loggin logic out of Blockchain class to testing script.

--- tests.js
+++ tests.js
@@ -1,14 +1,14 @@
 (function test(){
-	log("--- creating BlockchainDb instance, which is level DB wrapper.");
+	console.log("--- creating BlockchainDb instance, which is level DB wrapper.");
 	let db = new BlockchainDb("./db");
-	log("--- creating bc instance and injecting db intance to it.");
+	console.log("--- creating bc instance and injecting db intance to it.");
 	let bc = new Blockchain(db);
-	log("--- setting test constants.");
+	console.log("--- setting test constants.");
 	const TOTAL_BLOCKS = 20;
 	const ASYNC_WAIT_INTERVAL = 300;
 	const INVALID_BLOCK_HEIGHTS = [4, 9, 13];
 	
-	log("--- adding 20 blocks to the chain. 21 blocks total. 20 is the height of the latest block.")
+	console.log("--- adding 20 blocks to the chain. 21 blocks total. 20 is the height of the latest block.");
 	for(let i = 1; i <= TOTAL_BLOCKS; i++) {
 		let b = new Block("simple " + i);
 		setTimeout(() => {
@@ -16,25 +16,41 @@
 		}, i * ASYNC_WAIT_INTERVAL);
 	}
 	setTimeout(() => {
-		log("--- at this point the chain is valid => validateChain must log success message.");
-		bc.validateChain();
+		console.log("--- at this point the chain is valid => validateChain must log success message.");
+        bc.validateChain()
+            .then(status => {
+                // console.log(status);
+                console.log("chain is valid");
+            })
+            .catch(errors => {
+                console.log("chain is invalid");
+                errors.forEach(e => console.log(e));
+            });
 	}, (TOTAL_BLOCKS + 1) * ASYNC_WAIT_INTERVAL);
 	setTimeout(() => {
-		log("--- invalidating some blocks in the chain to test validation.");
+		console.log("--- invalidating some blocks in the chain to test validation.");
 		INVALID_BLOCK_HEIGHTS.forEach(index => {
-			log(`hacking block at ${index}`);
-			bc.db.getBlock(index).then(block => {
+			console.log(`hacking block at ${index}`);
+			bc.getChain().getBlock(index).then(block => {
 				block.body = `invalid block ${index}`;
-				return bc.db.db.put(index, JSON.stringify(block));
+				return bc.getChain().getDb().put(index, JSON.stringify(block));
 			}).then(result => {
-				log(`block hacked`);
+				console.log(`block hacked`);
 			}).catch(err => {
-				log(err);
+				console.log(err);
 			});
 		});
 	}, (TOTAL_BLOCKS + 2) * ASYNC_WAIT_INTERVAL);
 	setTimeout(() => {
-		log("--- at this point chain has 3 invalid blocks. => validateChain must log error message");
-		bc.validateChain();
+		console.log("--- at this point chain has 3 invalid blocks. => validateChain must return array with errors");
+		bc.validateChain()
+			.then(status => {
+				// console.log(status);
+				console.log("chain is valid");
+			})
+			.catch(errors => {
+                console.log("chain is invalid");
+                errors.forEach(e => console.log(e.message));
+			});
 	}, (TOTAL_BLOCKS + 3) * ASYNC_WAIT_INTERVAL);
 })();
